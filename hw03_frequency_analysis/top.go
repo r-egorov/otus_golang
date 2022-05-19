7b56f02b27c3836d739b1e8c05ed9a18f63234e8
Ruslan Egorov <r-egorov@outlook.com>
2022-05-19 17:35:31 +0300

    [HW03] Implement Top10 func, basic test

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,57 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type wordCounter struct {
+	word  string
+	count int
+}
+
+func Top10(inputStr string) []string {
+	words := strings.Fields(inputStr)
+
+	if len(words) == 0 {
+		return []string{}
+	}
+
+	wordCount := map[string]int{}
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	var wordCounters []wordCounter
+	for word, count := range wordCount {
+		wordCounters = append(wordCounters, wordCounter{word, count})
+	}
+
+	sort.Slice(wordCounters, func(left, right int) bool {
+		return wordCounters[left].count > wordCounters[right].count
+	})
+
+	wordCounters = wordCounters[:10]
+
+	fmt.Println(wordCounters)
+	for i := 0; i < 10; i++ {
+		if i < 9 && wordCounters[i+1].count == wordCounters[i].count {
+			j := i
+			for ; j < 9 && wordCounters[j].count == wordCounters[j+1].count; j++ {
+			}
+			subSlice := wordCounters[i : j+1]
+			sort.Slice(subSlice, func(left, right int) bool {
+				return subSlice[left].word < subSlice[right].word
+			})
+			i = j
+		}
+	}
+
+	var topWords []string
+	for _, wc := range wordCounters {
+		topWords = append(topWords, wc.word)
+	}
+
+	return topWords
 }
