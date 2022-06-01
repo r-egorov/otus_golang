5fa6d63e79a80400f8f4bedb6dbac9dd532f26f5
Ruslan Egorov <r-egorov@outlook.com>
2022-06-01 20:39:38 +0300

    [HW03] Implement cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -28,3 +28,47 @@ func NewCache(capacity int) Cache {
 		items:    make(map[Key]*ListItem, capacity),
 	}
 }
+
+func newCacheItem(key Key, value interface{}) *cacheItem {
+	return &cacheItem{
+		key:   key,
+		value: value,
+	}
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	wasInCache := false
+	if node, isInCache := l.items[key]; isInCache {
+		item := node.Value.(*cacheItem)
+		item.value = value
+		l.queue.MoveToFront(node)
+		wasInCache = true
+	} else {
+		item := newCacheItem(key, value)
+		if l.queue.Len() == l.capacity {
+			leastUsedNode := l.queue.Back()
+			leastUsedItem := leastUsedNode.Value.(*cacheItem)
+			delete(l.items, leastUsedItem.key)
+			l.queue.Remove(leastUsedNode)
+		}
+		node = l.queue.PushFront(item)
+		l.items[key] = node
+	}
+	return wasInCache
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	node, isInCache := l.items[key]
+	var res interface{} = nil
+	if isInCache {
+		l.queue.MoveToFront(node)
+		item := node.Value.(*cacheItem)
+		res = item.value
+	}
+	return res, isInCache
+}
+
+func (l *lruCache) Clear() {
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
+}
