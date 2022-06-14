50e164462d02a90fa0cef5ea3c5833e59d13924d
Ruslan Egorov <r-egorov@outlook.com>
2022-06-14 09:52:01 +0300

    [HW05] Test with require.Eventually

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -20,8 +20,10 @@ func TestRun(t *testing.T) {
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32
+		maxErrorsCount := 23
 
-		for i := 0; i < tasksCount; i++ {
+		// first m tasks return an error
+		for i := 0; i < maxErrorsCount; i++ {
 			err := fmt.Errorf("error from task %d", i)
 			tasks = append(tasks, func() error {
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
@@ -30,15 +32,23 @@ func TestRun(t *testing.T) {
 			})
 		}
 
+		// other tasks are cool
+		for i := maxErrorsCount; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
 		workersCount := 10
-		maxErrorsCount := 23
 		err := Run(tasks, workersCount, maxErrorsCount)
 
 		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
+	t.Run("tasks without errors using sleep", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -66,5 +76,47 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("tasks without errors using require.Eventually", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		done := make(chan bool)
+		go func() {
+			err := Run(tasks, workersCount, maxErrorsCount)
+			require.NoError(t, err)
+			done <- true
+			close(done)
+		}()
+
+		isFinished := func() bool {
+			select {
+			case <-done:
+				return true
+			default:
+				return false
+			}
+		}
+
+		require.Eventually(t, isFinished, sumTime/2, time.Millisecond*100, "tasks were run sequentially?")
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 	})
 }
