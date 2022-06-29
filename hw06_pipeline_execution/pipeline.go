c44e6e42640ce2caabbaadc960118d34eb68e9b9
Ruslan Egorov <r-egorov@outlook.com>
2022-06-29 13:37:58 +0300

    [HW06] Satisfy basic tests

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,57 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(Bi)
+
+	// Make an input channel for the first stage
+	// so that we can close it if need to
+	inFirstStage := make(Bi)
+
+	// Startup the stages,
+	// save input channel of the first stage
+	// save output channel of the last stage (going to be returned)
+	var inNextStage In = inFirstStage
+	var outLastStage Out
+	for _, stage := range stages {
+		outLastStage = stage(inNextStage)
+		inNextStage = outLastStage
+	}
+
+	// start sending values to the pipeline
+	go func() {
+		defer close(inFirstStage)
+
+		for inValue := range in {
+			select {
+			case inFirstStage <- inValue:
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	// start sending results to the output channel
+	// break if done is closed
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case result, more := <-outLastStage:
+				if more {
+					select {
+					case out <- result:
+					case <-done:
+						return
+					}
+				} else {
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return out
 }
