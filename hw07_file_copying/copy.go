5bc6171d589cb177263e58e7248443ce77097db6
Ruslan Egorov <r-egorov@outlook.com>
2022-07-06 11:57:57 +0300

    [HW07] Basic copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,64 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
+const bufferSize = 1
+
 var (
+	ErrSourceFileNotFound    = errors.New("source file not found")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Prepare source FD
+	sourceFd, err := os.OpenFile(fromPath, os.O_RDONLY, 0755)
+	if err != nil {
+		return err
+	}
+	fileStat, err := sourceFd.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > fileStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	lenToCopy := fileStat.Size() - offset
+	if limit > 0 && lenToCopy > limit {
+		lenToCopy = limit
+	}
+
+	_, err = sourceFd.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	// Prepare dest FD
+	destFd, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	err = copyContent(sourceFd, destFd, lenToCopy)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func copyContent(source io.Reader, dest io.Writer, lenToCopy int64) error {
+	var haveReadBytes int64
+	for haveReadBytes < lenToCopy {
+		readBytes, err := io.CopyN(dest, source, bufferSize)
+		if err != nil {
+			return err
+		}
+		haveReadBytes += readBytes
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,136 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	tmpDir        = "tmp"
+	tmpSourcename = "tmp.txt"
+	tmpDestname   = "tmpdest.txt"
+	testText      = "sometext"
+)
+
+type testCase struct {
+	sourceFile, destFile     *os.File
+	sourceText, expectedText string
+}
+
+func (t *testCase) tearDown() {
+	err := os.Remove(t.sourceFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.Remove(t.destFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setUpTestCase(sourceText, expectedText string) testCase {
+	tmpSourceFile, err := os.CreateTemp("", tmpSourcename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tmpSourceFile.Write([]byte(sourceText))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpDestFile, err := os.CreateTemp("", tmpDestname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return testCase{
+		sourceFile:   tmpSourceFile,
+		destFile:     tmpDestFile,
+		sourceText:   sourceText,
+		expectedText: expectedText,
+	}
+}
+
+func TestCopyContent(t *testing.T) {
+	t.Run("copies content of a reader to writer", func(t *testing.T) {
+		expected := testText
+		lenToCopy := int64(len(testText))
+
+		source := &bytes.Buffer{}
+		source.WriteString(testText)
+
+		dest := &bytes.Buffer{}
+		err := copyContent(source, dest, lenToCopy)
+
+		require.NoError(t, err)
+
+		got := dest.String()
+
+		require.Equal(t, expected, got)
+	})
+}
+
+func TestCopySuccess(t *testing.T) {
+	t.Run("copies from one file to another", func(t *testing.T) {
+		tc := setUpTestCase(testText, testText)
+
+		defer tc.tearDown()
+
+		err := Copy(tc.sourceFile.Name(), tc.destFile.Name(), 0, 0)
+
+		require.NoError(t, err)
+
+		body, err := os.ReadFile(tc.destFile.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		got := string(body)
+		require.Equal(t, tc.expectedText, got)
+	})
+
+	t.Run("copies with offset", func(t *testing.T) {
+		offset = 5
+		expected := testText[offset:]
+
+		tc := setUpTestCase(testText, expected)
+		defer tc.tearDown()
+
+		err := Copy(tc.sourceFile.Name(), tc.destFile.Name(), offset, 0)
+
+		require.NoError(t, err)
+
+		body, err := os.ReadFile(tc.destFile.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		got := string(body)
+		require.Equal(t, tc.expectedText, got)
+	})
+}
+
+func TestCopyFail(t *testing.T) {
+	t.Run("no source file", func(t *testing.T) {
+		tc := setUpTestCase(testText, "")
+		defer tc.tearDown()
+
+		err := Copy("invalidpath", tc.destFile.Name(), 0, 0)
+		require.Error(t, err, ErrSourceFileNotFound)
+	})
+
+	t.Run("offset is greater than the source file length", func(t *testing.T) {
+		var offset int64 = 9999999
+
+		tc := setUpTestCase(testText, "")
+		defer tc.tearDown()
+
+		err := Copy(tc.sourceFile.Name(), tc.destFile.Name(), offset, 0)
+
+		require.Error(t, err, ErrOffsetExceedsFileSize)
+	})
 }
