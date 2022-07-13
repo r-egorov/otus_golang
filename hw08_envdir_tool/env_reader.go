5583c1c7b7ee1b12d4a4845b46e5ae798a8a9912
Ruslan Egorov <r-egorov@outlook.com>
2022-07-13 13:44:35 +0300

    [HW08] Implement basic env_reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"os"
+	"path"
+	"strings"
+)
+
+const (
+	whiteSpaces    = " \n\t\v\f\r"
+	zeroTerminator = "\x00"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +22,55 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make(Environment, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		envValue, err := createEnvValueFromFile(dir, file)
+		if err != nil {
+			return nil, err
+		}
+
+		res[file.Name()] = envValue
+	}
+	return res, nil
+}
+
+func createEnvValueFromFile(dir string, file os.DirEntry) (EnvValue, error) {
+	fileInfo, err := file.Info()
+	if err != nil {
+		return EnvValue{}, err
+	}
+
+	preparedValue := ""
+	needRemove := false
+
+	if fileInfo.Size() == 0 {
+		needRemove = true
+	} else {
+		content, err := os.ReadFile(path.Join(dir, fileInfo.Name()))
+		if err != nil {
+			return EnvValue{}, err
+		}
+		preparedValue = prepareValue(string(content))
+	}
+	return EnvValue{
+		Value:      preparedValue,
+		NeedRemove: needRemove,
+	}, nil
+}
+
+func prepareValue(content string) string {
+	splitted := strings.Split(content, "\n")
+	firstLine := splitted[0]
+	firstLine = strings.TrimRight(firstLine, whiteSpaces)
+	firstLine = strings.Replace(firstLine, zeroTerminator, "\n", -1)
+
+	return firstLine
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,105 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("it reads the files in directory", func(t *testing.T) {
+		initialEnv := map[string]string{
+			"HELLO":     "hello",
+			"UNSET":     "",
+			"EMPTYLINE": "\nsecondline",
+			"SPACES":    "spaces \t\f ",
+			"ZEROTERM":  "zeroterminated\x00",
+		}
+		te := NewTestEnv(t)
+		defer te.tearDown(t)
+
+		te.addEnvVarFiles(t, initialEnv)
+
+		expected := expectedEnv(initialEnv)
+		got, err := ReadDir(te.tmpDirPath)
+
+		require.NoError(t, err)
+		require.Equal(t, expected, got)
+	})
+}
+
+type TestEnv struct {
+	tmpDirPath string
+}
+
+func NewTestEnv(t *testing.T) TestEnv {
+	t.Helper()
+
+	tmpDirPath, err := ioutil.TempDir("", "env_directory")
+	if err != nil {
+		t.Fatalf("can't create tempdir: %v", err)
+	}
+
+	return TestEnv{
+		tmpDirPath: tmpDirPath,
+	}
+}
+
+func (te *TestEnv) tearDown(t *testing.T) {
+	t.Helper()
+
+	err := os.RemoveAll(te.tmpDirPath)
+	if err != nil {
+		t.Fatalf("can't remove tempdir: %v", err)
+	}
+}
+
+func (te *TestEnv) addEnvVarFile(t *testing.T, key, value string) {
+	t.Helper()
+
+	filePath := path.Join(te.tmpDirPath, key)
+	file, err := os.Create(filePath)
+	defer file.Close()
+
+	if err != nil {
+		t.Fatalf("can't create env var file: %v", err)
+	}
+	_, err = file.Write([]byte(value))
+	if err != nil {
+		t.Fatalf("can't write to env var file: %v", err)
+	}
+}
+
+func (te *TestEnv) addEnvVarFiles(t *testing.T, envVars map[string]string) {
+	t.Helper()
+
+	for key, value := range envVars {
+		te.addEnvVarFile(t, key, value)
+	}
+}
+
+func expectedEnv(env map[string]string) Environment {
+	res := make(Environment)
+	for key, value := range env {
+		if len(value) == 0 {
+			res[key] = EnvValue{
+				Value:      value,
+				NeedRemove: true,
+			}
+		} else {
+			splitted := strings.Split(value, "\n")
+			firstLine := splitted[0]
+			firstLine = strings.TrimRight(firstLine, " \n\t\v\f\r")
+			firstLine = strings.Replace(firstLine, "\x00", "\n", -1)
+			res[key] = EnvValue{
+				Value:      firstLine,
+				NeedRemove: false,
+			}
+		}
+	}
+	return res
 }
