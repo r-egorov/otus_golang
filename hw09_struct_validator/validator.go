25470926f031c67f09d4e2c1aa40320ece7bcc97
Ruslan Egorov <r-egorov@outlook.com>
2022-07-22 21:02:13 +0300

    [HW09] Basic vbalidation for int and str

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,226 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrNotAStruct      = errors.New("parameter is not a struct")
+	ErrTagInvalid      = errors.New("invalid tag")
+	ErrTypeUnsupported = errors.New("unsupported type")
+
+	ErrStringLenInvalid    = errors.New("invalid string length")
+	ErrStringNotInSet      = errors.New("string not in set")
+	ErrStringNotRegexplike = errors.New("string not regexplike")
+
+	ErrIntUnderMinimum = errors.New("int less than minimum")
+	ErrIntOverMaximum  = errors.New("int greater than maximuim")
+	ErrIntNotInSet     = errors.New("int not in set")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
 }
 
+func (ve ValidationError) Error() string {
+	return fmt.Sprintf("{%s: %s}", ve.Field, ve.Err)
+}
+
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	if len(v) < 1 {
+		return "no validation errors"
+	}
+	builder := strings.Builder{}
+	builder.WriteString("Validation errors: ")
+	for i, err := range v {
+		builder.WriteString(err.Error())
+		if i < len(v)-1 {
+			builder.WriteString(", ")
+		}
+	}
+	return builder.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	valOfStruct := reflect.ValueOf(v)
+	typeOfStruct := reflect.TypeOf(v)
+	errs := ValidationErrors{}
+
+	if valOfStruct.Kind() != reflect.Struct {
+		return ErrNotAStruct
+	}
+
+	for i := 0; i < typeOfStruct.NumField(); i++ {
+		err := validateField(typeOfStruct.Field(i), valOfStruct.Field(i))
+		if err != nil {
+			errs = append(errs, *err)
+		}
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
+	return nil
+}
+
+func validateField(field reflect.StructField, fieldValue reflect.Value) *ValidationError {
+	tag, needToBeValidated := field.Tag.Lookup("validate")
+	if !needToBeValidated {
+		return nil
+	}
+
+	var err error
+	switch field.Type.Kind() {
+	case reflect.Slice:
+		err = processSlice(field, tag)
+	case reflect.String:
+		err = processString(fieldValue.String(), tag)
+	case reflect.Int:
+		err = processInt(int(fieldValue.Int()), tag)
+	default:
+		err = ErrTypeUnsupported
+	}
+
+	if err != nil {
+		validationErr := &ValidationError{
+			Field: field.Name,
+			Err:   err,
+		}
+		return validationErr
+	}
+	return nil
+}
+
+func processSlice(sl reflect.StructField, tag string) error {
+	elemType := sl.Type.Elem()
+	switch elemType.Kind() {
+	case reflect.String:
+		fmt.Println("slice of strings")
+	case reflect.Int:
+		fmt.Println("slice of ints")
+	default:
+		fmt.Println("slice not supported")
+	}
+	return nil
+}
+
+func processString(str string, tag string) error {
+	tagSplitted := strings.SplitN(tag, ":", 2)
+	if len(tagSplitted) != 2 {
+		return ErrTagInvalid
+	}
+	tagKey := tagSplitted[0]
+	tagValue := tagSplitted[1]
+
+	var err error
+	switch tagKey {
+	case "len":
+		expectedLen, atoiErr := strconv.Atoi(tagValue)
+		if atoiErr != nil {
+			return ErrTagInvalid
+		}
+		err = checkStringLen(str, expectedLen)
+	case "in":
+		err = checkStringInSet(str, strings.Split(tagValue, ","))
+	case "regexp":
+		err = checkStringRegexplike(str, tagValue)
+	default:
+		err = ErrTagInvalid
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkStringLen(str string, expectedLen int) error {
+	if len([]rune(str)) != expectedLen {
+		return ErrStringLenInvalid
+	}
+	return nil
+}
+
+func checkStringInSet(str string, set []string) error {
+	if len(set) < 1 {
+		return ErrTagInvalid
+	}
+	for _, s := range set {
+		if str == s {
+			return nil
+		}
+	}
+	return ErrStringNotInSet
+}
+
+func checkStringRegexplike(str string, regexStr string) error {
+	regex, err := regexp.Compile(regexStr)
+	if err != nil {
+		return ErrTagInvalid
+	}
+
+	matched := regex.MatchString(str)
+	if !matched {
+		return ErrStringNotRegexplike
+	}
+	return nil
+}
+
+func processInt(fieldValue int, tag string) error {
+	tagSplitted := strings.SplitN(tag, ":", 2)
+	if len(tagSplitted) != 2 {
+		return ErrTagInvalid
+	}
+	tagKey := tagSplitted[0]
+	tagValue := tagSplitted[1]
+
+	var err error
+	switch tagKey {
+	case "min":
+		minimum, atoiErr := strconv.Atoi(tagValue)
+		if atoiErr != nil {
+			return ErrTagInvalid
+		}
+		if fieldValue < minimum {
+			return ErrIntUnderMinimum
+		}
+	case "max":
+		maximum, atoiErr := strconv.Atoi(tagValue)
+		if atoiErr != nil {
+			return ErrTagInvalid
+		}
+		if fieldValue > maximum {
+			return ErrIntOverMaximum
+		}
+	case "in":
+		intsStrs := strings.Split(tagValue, ",")
+		if len(intsStrs) < 1 {
+			return ErrTagInvalid
+		}
+		for i := 0; i < len(intsStrs); i++ {
+			atoied, atoiErr := strconv.Atoi(intsStrs[i])
+			if atoiErr != nil {
+				return ErrTagInvalid
+			}
+			if fieldValue == atoied {
+				return nil
+			}
+		}
+		return ErrIntNotInSet
+	default:
+		err = ErrTagInvalid
+	}
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,10 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,9 +15,9 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int      `validate:"min:18|max:50"`
+		Age    int      `validate:"min:18"` // `validate:"min:18|max:50"`
 		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole `validate:"in:admin,stuff"`
+		Role   UserRole `validate:"in:admin,staff"`
 		Phones []string `validate:"len:11"`
 		meta   json.RawMessage
 	}
@@ -34,26 +36,191 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
-)
 
-func TestValidate(t *testing.T) {
-	tests := []struct {
-		in          interface{}
-		expectedErr error
-	}{
-		{
-			// Place your code here.
-		},
-		// ...
-		// Place your code here.
+	Tweet struct {
+		message string `validate:"len:50"`
+		sender  string `validate:"in:user,admin"`
 	}
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+	IntCase struct {
+		minimum int `validate:"min:50"`
+		maximum int `validate:"max:100"`
+		in      int `validate:"in:42"`
+	}
+
+	testCase struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}
+)
+
+func TestValidateComplex(t *testing.T) {
+	tests := []testCase{
+		{
+			name: "Complex OK",
+			in: User{
+				ID:     "824e3916-bc8f-4f95-9dd0-d8f62e3b36e7",
+				Name:   "John Doe",
+				Age:    18,
+				Email:  "example@mail.com",
+				Role:   "staff",
+				Phones: []string{"79234324123", "1111111111"},
+				meta:   json.RawMessage(`{"test": "user"}`),
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "Complex KO",
+			in: User{
+				ID:     "824e3916-bc8f-4f95-9dd0", // too short
+				Name:   "John Doe",
+				Age:    10,                // not in set
+				Email:  "examplemail.com", // not valid email
+				Role:   "meremortal",      // not in
+				Phones: []string{"79234324123", "1111111111"},
+				meta:   json.RawMessage(`{"test": "user"}`),
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "ID",
+					Err:   ErrStringLenInvalid,
+				},
+				ValidationError{
+					Field: "Age",
+					Err:   ErrIntUnderMinimum,
+				},
+				ValidationError{
+					Field: "Email",
+					Err:   ErrStringNotRegexplike,
+				},
+				ValidationError{
+					Field: "Role",
+					Err:   ErrStringNotInSet,
+				},
+			},
+		},
+	}
+
+	runTests(t, tests)
+}
+
+func TestValidateString(t *testing.T) {
+	tests := []testCase{
+		{
+			name: "String KO - too short",
+			in: Tweet{
+				message: "Can't you think of even 50 symbols? Pathetic.",
+				sender:  "user",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "message",
+					Err:   ErrStringLenInvalid,
+				},
+			},
+		},
+		{
+			name: "String KO - too long",
+			in: Tweet{
+				message: "Wow, that's a good job you've done here, bravo. You outdid yourself.",
+				sender:  "user",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "message",
+					Err:   ErrStringLenInvalid,
+				},
+			},
+		},
+		{
+			name: "String KO - not in set",
+			in: Tweet{
+				message: "Wow, that's a good job you've done here, bravo. Yo",
+				sender:  "justsomedude",
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "sender",
+					Err:   ErrStringNotInSet,
+				},
+			},
+		},
+	}
+	runTests(t, tests)
+}
+
+func TestValidateInt(t *testing.T) {
+	tests := []testCase{
+		{
+			name: "under minimum",
+			in: IntCase{
+				minimum: 1,
+				maximum: 70,
+				in:      42,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "minimum",
+					Err:   ErrIntUnderMinimum,
+				},
+			},
+		},
+		{
+			name: "over maximum",
+			in: IntCase{
+				minimum: 100,
+				maximum: 250,
+				in:      42,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "maximum",
+					Err:   ErrIntOverMaximum,
+				},
+			},
+		},
+		{
+			name: "not in set",
+			in: IntCase{
+				minimum: 100,
+				maximum: 100,
+				in:      100500,
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{
+					Field: "in",
+					Err:   ErrIntNotInSet,
+				},
+			},
+		},
+	}
+	runTests(t, tests)
+}
+
+func runTests(t *testing.T, testCases []testCase) {
+	t.Helper()
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
+			errs := Validate(tt.in)
+			if tt.expectedErr == nil {
+				require.NoError(t, errs)
+			} else {
+				var valErrs ValidationErrors
+				if errors.As(errs, &valErrs) {
+					var expectedErrs ValidationErrors
+					require.ErrorAs(t, tt.expectedErr, &expectedErrs)
+					for i, err := range valErrs {
+						require.ErrorIs(t, err, expectedErrs[i])
+					}
+				} else {
+					require.ErrorIs(t, errs, tt.expectedErr)
+				}
+			}
 			_ = tt
 		})
 	}
