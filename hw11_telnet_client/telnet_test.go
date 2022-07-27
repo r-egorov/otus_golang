5435d921c40112b1cf2b5cf31bd37bf40a11c1d3
Ruslan Egorov <r-egorov@outlook.com>
2022-07-27 12:51:31 +0300

    [HW11] Basic tests passed

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -11,55 +11,68 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type testCase struct {
+	name, sendInTelnet, sendFromTelnet string
+}
+
 func TestTelnetClient(t *testing.T) {
-	t.Run("basic", func(t *testing.T) {
-		l, err := net.Listen("tcp", "127.0.0.1:")
-		require.NoError(t, err)
-		defer func() { require.NoError(t, l.Close()) }()
-
-		var wg sync.WaitGroup
-		wg.Add(2)
-
-		go func() {
-			defer wg.Done()
-
-			in := &bytes.Buffer{}
-			out := &bytes.Buffer{}
-
-			timeout, err := time.ParseDuration("10s")
+	testCases := []testCase{
+		{
+			name:           "basic",
+			sendFromTelnet: "hello\n",
+			sendInTelnet:   "world\n",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			l, err := net.Listen("tcp", "127.0.0.1:")
 			require.NoError(t, err)
+			defer func() { require.NoError(t, l.Close()) }()
 
-			client := NewTelnetClient(l.Addr().String(), timeout, ioutil.NopCloser(in), out)
-			require.NoError(t, client.Connect())
-			defer func() { require.NoError(t, client.Close()) }()
+			var wg sync.WaitGroup
+			wg.Add(2)
 
-			in.WriteString("hello\n")
-			err = client.Send()
-			require.NoError(t, err)
+			go func() {
+				defer wg.Done()
 
-			err = client.Receive()
-			require.NoError(t, err)
-			require.Equal(t, "world\n", out.String())
-		}()
+				in := &bytes.Buffer{}
+				out := &bytes.Buffer{}
 
-		go func() {
-			defer wg.Done()
+				timeout, err := time.ParseDuration("10s")
+				require.NoError(t, err)
 
-			conn, err := l.Accept()
-			require.NoError(t, err)
-			require.NotNil(t, conn)
-			defer func() { require.NoError(t, conn.Close()) }()
+				client := NewTelnetClient(l.Addr().String(), timeout, ioutil.NopCloser(in), out)
+				require.NoError(t, client.Connect())
+				defer func() { require.NoError(t, client.Close()) }()
 
-			request := make([]byte, 1024)
-			n, err := conn.Read(request)
-			require.NoError(t, err)
-			require.Equal(t, "hello\n", string(request)[:n])
+				in.WriteString(tc.sendFromTelnet)
+				err = client.Send()
+				require.NoError(t, err)
 
-			n, err = conn.Write([]byte("world\n"))
-			require.NoError(t, err)
-			require.NotEqual(t, 0, n)
-		}()
+				err = client.Receive()
+				require.NoError(t, err)
+				require.Equal(t, tc.sendInTelnet, out.String())
+			}()
 
-		wg.Wait()
-	})
+			go func() {
+				defer wg.Done()
+
+				conn, err := l.Accept()
+				require.NoError(t, err)
+				require.NotNil(t, conn)
+				defer func() { require.NoError(t, conn.Close()) }()
+
+				request := make([]byte, 1024)
+				n, err := conn.Read(request)
+				require.NoError(t, err)
+				require.Equal(t, tc.sendFromTelnet, string(request)[:n])
+
+				n, err = conn.Write([]byte(tc.sendInTelnet))
+				require.NoError(t, err)
+				require.NotEqual(t, 0, n)
+			}()
+
+			wg.Wait()
+		})
+	}
 }
