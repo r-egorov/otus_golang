85091ae70b7f4742589e39bd167e2eb39b5976de
Ruslan Egorov <r-egorov@outlook.com>
2022-08-04 22:44:33 +0300

    [HW12] SQLStorage Tested

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jinzhu/now v1.1.5
 	github.com/lib/pq v1.10.6
 	github.com/spf13/viper v1.12.0
 	github.com/stretchr/testify v1.8.0
--- hw12_13_14_15_calendar/internal/storage/sql/storage_test.go
+++ hw12_13_14_15_calendar/internal/storage/sql/storage_test.go
@@ -6,7 +6,6 @@ package sqlstorage
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
@@ -27,12 +26,11 @@ func TestSQLStorage_SaveEvent(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, event, gotEvent)
 
-		gotEvent, err = selectEvent(s.db, event.ID)
-		require.NoError(t, err)
-		require.Equal(t, event, gotEvent)
+		assertEventInDB(t, s.db, event)
 	})
 
 	t.Run("it returns err if ID is not unique", func(t *testing.T) {
+		defer truncateTable(t, s.db)
 		refEvent := generateEvent()
 		_, err := s.SaveEvent(ctx, refEvent)
 		require.NoError(t, err)
@@ -44,12 +42,11 @@ func TestSQLStorage_SaveEvent(t *testing.T) {
 		var errIDNotUnique *storage.ErrIDNotUnique
 		require.ErrorAs(t, err, &errIDNotUnique)
 
-		eventInDB, err := selectEvent(s.db, refEvent.ID)
-		require.NoError(t, err)
-		require.Equal(t, refEvent, eventInDB)
+		assertEventInDB(t, s.db, refEvent)
 	})
 
 	t.Run("it returns err if date is busy", func(t *testing.T) {
+		defer truncateTable(t, s.db)
 		refEvent := generateEvent()
 		_, err := s.SaveEvent(ctx, refEvent)
 		require.NoError(t, err)
@@ -61,9 +58,273 @@ func TestSQLStorage_SaveEvent(t *testing.T) {
 		var errDateBusy *storage.ErrDateBusy
 		require.ErrorAs(t, err, &errDateBusy)
 
-		eventInDB, err := selectEvent(s.db, refEvent.ID)
+		assertEventInDB(t, s.db, refEvent)
+	})
+}
+
+func TestStorage_UpdateEvent(t *testing.T) {
+	s := New("postgres", "postgres", "calendar_test", "localhost", "5432")
+	ctx := context.Background()
+	require.NoError(t, s.Connect(ctx))
+
+	t.Run("basic", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		event := generateEvent()
+		_, err := s.SaveEvent(ctx, event)
 		require.NoError(t, err)
-		require.Equal(t, refEvent, eventInDB)
+
+		event.Title = "WOW THAT'S NEW TITLE"
+		event.Duration = 16 * time.Minute
+		event.Description = "DESCRIPTION? ALSO NEW!"
+
+		updEvent, err := s.UpdateEvent(ctx, event)
+		require.NoError(t, err)
+		require.Equal(t, event, updEvent)
+
+		assertEventInDB(t, s.db, event)
+	})
+
+	t.Run("it returns err if the date is busy", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		busyEvent := generateEvent()
+		_, err := s.SaveEvent(ctx, busyEvent)
+		require.NoError(t, err)
+
+		newEvent := generateEvent()
+		newEvent.DateTime = newEvent.DateTime.Add(time.Hour * 48)
+		newEvent.OwnerID = busyEvent.OwnerID
+		_, err = s.SaveEvent(ctx, newEvent)
+		require.NoError(t, err)
+
+		newEventUpdated := newEvent
+		newEventUpdated.DateTime = busyEvent.DateTime
+		_, err = s.UpdateEvent(ctx, newEventUpdated)
+		var errDateBusy *storage.ErrDateBusy
+		require.ErrorAs(t, err, &errDateBusy)
+
+		assertEventInDB(t, s.db, newEvent)
+	})
+
+	t.Run("it returns err if ID is not found", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		newEvent := generateEvent()
+		_, err := s.UpdateEvent(ctx, newEvent)
+		require.Error(t, err)
+		var errIDNotFound *storage.ErrIDNotFound
+		require.ErrorAs(t, err, &errIDNotFound)
+	})
+}
+
+func TestStorage_DeleteEvent(t *testing.T) {
+	s := New("postgres", "postgres", "calendar_test", "localhost", "5432")
+	ctx := context.Background()
+	require.NoError(t, s.Connect(ctx))
+
+	t.Run("basic", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		eventInDB := generateEvent()
+		_, err := s.SaveEvent(ctx, eventInDB)
+		require.NoError(t, err)
+
+		err = s.DeleteEvent(ctx, eventInDB.ID)
+		require.NoError(t, err)
+
+		_, err = selectEvent(s.db, eventInDB.ID)
+		require.Error(t, err)
+		var errIDNotFound *storage.ErrIDNotFound
+		require.ErrorAs(t, err, &errIDNotFound)
+	})
+
+	t.Run("it returns err if id not found", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		err := s.DeleteEvent(ctx, uuid.New())
+		require.Error(t, err)
+		var errIDNotFound *storage.ErrIDNotFound
+		require.ErrorAs(t, err, &errIDNotFound)
+	})
+
+}
+
+func TestStorage_ListEventsDay(t *testing.T) {
+	s := New("postgres", "postgres", "calendar_test", "localhost", "5432")
+	ctx := context.Background()
+	require.NoError(t, s.Connect(ctx))
+
+	t.Run("it returns list of events on a day", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+
+		date := time.Date(2022, time.Month(3), 16, 0, 0, 0, 0, time.UTC)
+		morning := time.Date(2022, time.Month(3), 16, 6, 30, 0, 0, time.UTC)
+		afternoon := time.Date(2022, time.Month(3), 16, 15, 30, 0, 0, time.UTC)
+		evening := time.Date(2022, time.Month(3), 16, 21, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = morning
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = afternoon
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		eventThree := generateEvent()
+		eventThree.DateTime = evening
+		_, err = s.SaveEvent(ctx, eventThree)
+		require.NoError(t, err)
+
+		otherEvent := generateEvent()
+		_, err = s.SaveEvent(ctx, otherEvent)
+		require.NoError(t, err)
+
+		expected := []storage.Event{eventOne, eventTwo, eventThree}
+		got, err := s.ListEventsDay(ctx, date)
+		require.NoError(t, err)
+		assertEventListsEqual(t, expected, got)
+	})
+
+	t.Run("it returns empty list of events", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+
+		morning := time.Date(2022, time.Month(3), 1, 6, 30, 0, 0, time.UTC)
+		afternoon := time.Date(2022, time.Month(3), 1, 15, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = morning
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = afternoon
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		date := time.Date(2022, time.Month(12), 12, 0, 0, 0, 0, time.UTC)
+		got, err := s.ListEventsDay(ctx, date)
+		require.NoError(t, err)
+		require.Equal(t, []storage.Event{}, got)
+	})
+}
+
+func TestStorage_ListEventsWeek(t *testing.T) {
+	s := New("postgres", "postgres", "calendar_test", "localhost", "5432")
+	ctx := context.Background()
+	require.NoError(t, s.Connect(ctx))
+
+	t.Run("it returns list of events in a week", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+
+		weekStart := time.Date(2022, time.Month(3), 7, 0, 0, 0, 0, time.UTC)
+		thursday := time.Date(2022, time.Month(3), 10, 6, 30, 0, 0, time.UTC)
+		friday := time.Date(2022, time.Month(3), 11, 15, 30, 0, 0, time.UTC)
+		tuesday := time.Date(2022, time.Month(3), 8, 21, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = tuesday
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = friday
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		eventThree := generateEvent()
+		eventThree.DateTime = thursday
+		_, err = s.SaveEvent(ctx, eventThree)
+		require.NoError(t, err)
+
+		otherEvent := generateEvent()
+		_, err = s.SaveEvent(ctx, otherEvent)
+		require.NoError(t, err)
+
+		expected := []storage.Event{eventOne, eventTwo, eventThree}
+		got, err := s.ListEventsWeek(ctx, weekStart)
+		require.NoError(t, err)
+		assertEventListsEqual(t, expected, got)
+	})
+
+	t.Run("it returns empty list of events", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+
+		thursday := time.Date(2022, time.Month(3), 10, 6, 30, 0, 0, time.UTC)
+		friday := time.Date(2022, time.Month(3), 11, 15, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = thursday
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = friday
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		weekStart := time.Date(2022, time.Month(10), 10, 0, 0, 0, 0, time.UTC)
+		got, err := s.ListEventsDay(ctx, weekStart)
+		require.NoError(t, err)
+		require.Equal(t, []storage.Event{}, got)
+	})
+}
+
+func TestStorage_ListEventsMonth(t *testing.T) {
+	s := New("postgres", "postgres", "calendar_test", "localhost", "5432")
+	ctx := context.Background()
+	require.NoError(t, s.Connect(ctx))
+
+	t.Run("it returns list of events in a month", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+
+		date := time.Date(2022, time.Month(3), 1, 0, 0, 0, 0, time.UTC)
+		tenth := time.Date(2022, time.Month(3), 10, 6, 30, 0, 0, time.UTC)
+		fifteenth := time.Date(2022, time.Month(3), 15, 15, 30, 0, 0, time.UTC)
+		twentyfifth := time.Date(2022, time.Month(3), 25, 21, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = tenth
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = fifteenth
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		eventThree := generateEvent()
+		eventThree.DateTime = twentyfifth
+		_, err = s.SaveEvent(ctx, eventThree)
+		require.NoError(t, err)
+
+		otherEvent := generateEvent()
+		_, err = s.SaveEvent(ctx, otherEvent)
+		require.NoError(t, err)
+
+		expected := []storage.Event{eventOne, eventTwo, eventThree}
+		got, err := s.ListEventsMonth(ctx, date)
+		require.NoError(t, err)
+		assertEventListsEqual(t, expected, got)
+	})
+
+	t.Run("it returns empty list of events", func(t *testing.T) {
+		defer truncateTable(t, s.db)
+		
+		tenth := time.Date(2022, time.Month(3), 10, 6, 30, 0, 0, time.UTC)
+		fifteenth := time.Date(2022, time.Month(3), 15, 15, 30, 0, 0, time.UTC)
+
+		eventOne := generateEvent()
+		eventOne.DateTime = tenth
+		_, err := s.SaveEvent(ctx, eventOne)
+		require.NoError(t, err)
+
+		eventTwo := generateEvent()
+		eventTwo.DateTime = fifteenth
+		_, err = s.SaveEvent(ctx, eventTwo)
+		require.NoError(t, err)
+
+		date := time.Date(2022, time.Month(12), 1, 0, 0, 0, 0, time.UTC)
+		got, err := s.ListEventsDay(ctx, date)
+		require.NoError(t, err)
+		require.Equal(t, []storage.Event{}, got)
 	})
 }
 
@@ -93,7 +354,7 @@ func selectEvent(db *sql.DB, eventID uuid.UUID) (storage.Event, error) {
 
 	switch err {
 	case sql.ErrNoRows:
-		return event, fmt.Errorf("no event with id %s", eventID.String())
+		return event, storage.NewErrIDNotFound(eventID)
 	case nil:
 		if sqlDuration.Valid {
 			event.Duration = time.Duration(sqlDuration.Int64)
@@ -116,4 +377,27 @@ func generateEvent() storage.Event {
 		Description: "New description",
 		OwnerID:     uuid.New(),
 	}
+}
+
+func assertEventInDB(t *testing.T, db *sql.DB, refEvent storage.Event) {
+	eventInDB, err := selectEvent(db, refEvent.ID)
+	require.NoError(t, err)
+	require.Equal(t, refEvent, eventInDB)
+}
+
+func assertEventListsEqual(t *testing.T, l, r []storage.Event) {
+	t.Helper()
+
+	mapFromSlice := func(sl []storage.Event) map[uuid.UUID]storage.Event {
+		res := make(map[uuid.UUID]storage.Event)
+		for _, event := range sl {
+			res[event.ID] = event
+		}
+		return res
+	}
+
+	lmap := mapFromSlice(l)
+	rmap := mapFromSlice(r)
+
+	require.Equal(t, lmap, rmap)
 }
