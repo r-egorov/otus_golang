4d9105e4db46ee4937086e75020e535b445801d0
Ruslan Egorov <r-egorov@outlook.com>
2022-09-15 16:11:42 +0300

    [HW14] Add notification entity

--- hw12_13_14_15_calendar/internal/storage/notification.go
+++ hw12_13_14_15_calendar/internal/storage/notification.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+)
+
+type Notification struct {
+	EventID    uuid.UUID `json:"eventId"`
+	EventTitle string    `json:"eventTitle"`
+	DateTime   time.Time `json:"datetime"`
+	OwnerID    uuid.UUID `json:"ownerId"`
+}
+
+func (n *Notification) String() string {
+	return fmt.Sprintf("Notification<EventID: %s, EventTitle: %s>",
+		n.EventID.String(), n.EventTitle)
+}
